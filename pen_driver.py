27ff78b2bb58156e64350da0c4df6f560444d735
Gustavo Chavez <cascade144@live.com>
2018-06-08 14:36:22 -0500

    Added driver and scanner module for tool.

---
+++ pen_driver.py
@@ -0,0 +1,20 @@
+"""
+File: controllerDriver.py
+By: Gustavo Chavez
+"""
+
+# IMPORTS ###
+import scapy
+
+
+# GLOBALS ###
+header = '*******************************************'
+
+def main():
+    print(header)
+    print('This is a pen tool for MQTT')
+    print(header)
+    
+
+if __name__ == '__main__':
+    main()
