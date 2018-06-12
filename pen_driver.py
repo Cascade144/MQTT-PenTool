170009e63d50cc4df3f8a501fc53a073b2b35411
Gustavo Chavez <cascade144@live.com>
2018-06-12 15:38:42 -0500

    Added files for scanner and probe parts of tool.

--- pen_driver.py
+++ pen_driver.py
@@ -4,16 +4,31 @@ By: Gustavo Chavez
 """
 
 # IMPORTS ###
-import scapy
+import argparse
+import sys
 
+# LOCAL IMPORTS ###
+from scanner import arp_scan, port_scan
 
 # GLOBALS ###
 header = '*******************************************'
+
 
 def main():
     print(header)
     print('This is a pen tool for MQTT')
     print(header)
+    if len(sys.argv) == 1:
+        print('Usage: python3 pen_driver.py [-s]')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', action='store_true')
+    args = parser.parse_args()
+    if args.s:
+        print('Beginning scan for MQTT Brokers.')
+        arp_scan()
+        print('Attempting to find default port 1883 and 8883 open on responsive hosts.')
+        port_scan()
+
     
 
 if __name__ == '__main__':
--- scanner.py
+++ scanner.py
@@ -1,4 +1,56 @@
-import scapy
-import logging
+import scapy.all as scapy
+from scapy.layers.l2 import Ether, ARP
+from scapy.layers.inet import IP, TCP
+# import scapy.all. as Ether
 
-def scan():
+import logging
+import socket
+
+
+interface = None
+# Packet list of open hosts
+open_hosts = []
+
+
+def arp_scan():
+    print('')
+    try:
+        interface = input('[*] Enter interface to scan on: ')
+        net_addr = input('[*] Enter network address with net mask: ')
+        print('[*] Scanning...')
+        ans,unans = scapy.srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=net_addr), iface=interface, \
+                              timeout=2, inter=0.1)
+        print(ans)
+        ans.display()
+        print(unans)
+        print('MAC - IP')
+        for send, recv in ans:
+            print(recv[Ether].src+' - '+recv[ARP].psrc)
+            open_hosts.append(recv)
+        print('[*] Scan complete')
+        print('Result')
+    except KeyboardInterrupt:
+        print()
+        print('[*] Quitting')
+
+
+def port_scan():
+    print('')
+    try:
+        for host in open_hosts:
+            dst_ip = str(host[ARP].psrc)
+            src_port = 50
+            dst_port = 1883
+            response = scapy.sr1(IP(dst=dst_ip)/TCP(sport=src_port, dport=dst_port, flags='S'), timeout=10)
+            if str(type(response)) == '<class \'NoneType\'>':
+                print('Syn Request filtered')
+            elif response.haslayer(TCP):
+                if response.getlayer(TCP).flags == 0x12:
+                    scapy.sr(IP(dst=dst_ip)/TCP(sport=src_port, dport=dst_port, flags='R'), timeout=10)
+                    print('Host '+dst_ip+' has port '+str(dst_port)+' open')
+                elif response.getlayer(TCP).flags == 0x14:
+                    print('Host '+dst_ip+' has port '+str(dst_port)+' closed')
+
+    except KeyboardInterrupt:
+        print()
+        print('[*] Quitting')
